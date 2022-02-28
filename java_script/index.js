740660f609792c668d7e48a130f929a7c2fa49cd
Bokyeom_Kim <96895747+BokyeomKim@users.noreply.github.com>
2022-02-28 15:42:28 +0900

    Add files via upload

---
+++ java_script/index.js
@@ -0,0 +1,27 @@
+//자바 print
+// System.out.println("Hello World");
+//자바스크립트 print
+//세미콜론이 없어도 실행이 된다. (권장사양)
+//java 와 같은 데이터 타입이 없다. 
+console.log('Hello World');
+var x = 10;
+var y = '안녕하세요';
+console.log('x의 값은? '+x);
+console.log('y의 값은? '+y);
+
+
+var num =10;
+var num2 =20;
+var result = num+num2;
+console.log(result);
+if(result==30){
+    console.log('30입니다.')
+}
+for(var i=0; i<10; i++){
+    console.log(i)
+}
+for(var i=1; i<=10; i++){
+    if(i%2==0){
+        console.log(i+ "는 짝수 입니다.")
+    }
+}
