f054749dc5fc4c323f017d1b978a3f0c99007fcc
junsu_Kim <96895747+junsu22@users.noreply.github.com>
2022-04-05 16:15:24 +0900

    상속 연습 0405

---
+++ JAVA/생성자 연습폴더/practice.java
@@ -0,0 +1,79 @@
+package 상속;
+
+class Ga {
+	public void 연습하다() {
+		System.out.println("가.연습하는중(1)");
+	}
+}
+
+class Na {
+	public void 연습하다() {
+		System.out.println("나.연습하는중(2)");
+	}
+}
+
+class Da {
+
+	public void 연습하다() {
+		System.out.println("다.연습하는중(3)");
+	}
+}
+
+class La {
+	public void 연습하다() {
+		System.out.println("라.연습하는중(4)");
+	}
+}
+
+class Ma {
+	public void 연습하다() {
+		System.out.println("마.연습하는중(5)");
+	}
+}
+
+class Ba {
+	public void 연습하다() {
+		System.out.println("바.연습하는중(6)");
+	}
+}
+
+class Sa {
+	public void 연습하다() {
+		System.out.println("사.연습하는중(7)");
+	}
+}
+
+class Exit{
+	public void 종료하다() {
+		System.out.println("연습을 종료 합니다!");
+	}
+}
+
+public class practice {
+
+	public static void main(String[] args) {
+		Ga a = new Ga();
+		a.연습하다();
+
+		Na b = new Na();
+		b.연습하다();
+
+		Da c = new Da();
+		c.연습하다();
+
+		La d = new La();
+		d.연습하다();
+
+		Ma e = new Ma();
+		e.연습하다();
+
+		Ba f = new Ba();
+		f.연습하다();
+		Sa g = new Sa();
+		g.연습하다();
+		Exit exit = new Exit();
+		exit.종료하다();
+	
+	}
+
+}
