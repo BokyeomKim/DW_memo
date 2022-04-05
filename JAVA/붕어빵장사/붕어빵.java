873345ea2fffec28c9f0318caa8bf7621eec7489
junsu_Kim <96895747+junsu22@users.noreply.github.com>
2022-04-05 12:42:13 +0900

    Add files via upload

---
+++ JAVA/붕어빵장사/붕어빵.java
@@ -0,0 +1,13 @@
+package »ó¼Ó;
+
+public class ºØ¾î»§ {
+	String type;
+	int count;
+
+	public ºØ¾î»§(String type, int count) {
+
+		this.type = type;
+		this.count = count;
+
+	}
+}
---
+++ JAVA/붕어빵장사/붕어빵장사.java
@@ -0,0 +1,17 @@
+package »ó¼Ó;
+
+public class ºØ¾î»§Àå»ç {
+
+	public static void main(String[] args) {
+		ºØ¾î»§ ÆÏºØ¾î = new ºØ¾î»§("ÆÏ", 10);
+		System.out.println(ÆÏºØ¾î.type + "," + ÆÏºØ¾î.count);
+
+		ºØ¾î»§ ½´Å©¸²ºØ¾î = new ºØ¾î»§("½´Å©¸²", 20);
+		System.out.println(½´Å©¸²ºØ¾î.type + "," + ½´Å©¸²ºØ¾î.count);
+
+		ºØ¾î»§ ÇÇÀÚºØ¾î = new ºØ¾î»§("ÇÇÀÚ", 30);
+		System.out.println(ÇÇÀÚºØ¾î.type + "," + ÇÇÀÚºØ¾î.count);
+
+	}
+
+}
