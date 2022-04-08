36174ce76e05b7792a78b5f8c8870c357db296e7
junsu_Kim <96895747+junsu22@users.noreply.github.com>
2022-04-08 12:27:46 +0900

    Add files via upload

---
+++ JAVA/Practice_folder_for_JAVA/생성자/Person.java
@@ -0,0 +1,8 @@
+package 연습;
+
+class Person {
+    //person class 에
+    int age;//int 형 
+Person(int number){age= number;}
+//int 형 매개변수를 받아 age 값을 초기화
+}
---
+++ JAVA/Practice_folder_for_JAVA/생성자/PersonExam.java
@@ -0,0 +1,9 @@
+package 연습;
+
+public class PersonExam {
+	public static void main(String[] args) {	
+		 //Person클래스에서 int형 변수를 매개변수로 받는 생성자를 호출
+        Person person = new Person(25);
+	}
+
+}
