4ebe70b7489cdee248060ff05f584500caf77060
junsu_Kim <96895747+junsu22@users.noreply.github.com>
2022-04-12 16:13:00 +0900

    Add files via upload

---
+++ Hash_Study.java
@@ -0,0 +1,36 @@
+package 컬렉션즈;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+class OneSoju{
+	
+}
+
+public class Hash_Study {
+
+	public static void main(String[] args) {
+
+		ArrayList<String>list = new ArrayList<String>();
+		List<String> list2 = new ArrayList<String>();
+		List2 = new LinkedList<String>();
+		
+		OneSoju onesoju= new OneSoju();
+			
+		HashMap<String,Object> map = new HashMap<String,Object>();
+		
+		HashMap<String,Object> map2 = new HashMap<String,Object>();
+		
+		
+		map.put("1", "참이슬"); // Hashmap 에 데이터 넣기 !
+		map.put("2", "처음처럼");
+		map.put("a", "한라산");
+		map.put("b", 13);  
+		System.out.println(map.get("1"));// key이름으로 value 접근 
+		//문제1 . 한라산 출력하기 
+		System.out.println(map.get("a"));
+	}
+
+}
